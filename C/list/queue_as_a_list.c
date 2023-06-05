1997a54aa552de9563df62ceca6e500fbc22d81b
Learner-sujan <poudelsujan03@gmail.com>
2023-06-05 23:18:30 +0545

    queue as a list update

--- C/list/queue_as_a_list.c
+++ C/list/queue_as_a_list.c
@@ -1,85 +1,105 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-struct Node {
-    int data;
-    struct Node* next;
-};
+#define MAX_SIZE 100
 
 struct Queue {
-    struct Node* front;
-    struct Node* rear;
+    int list[MAX_SIZE];
+    int front;
+    int rear;
 };
 
-struct Queue* createQueue() {
-    struct Queue* queue = (struct Queue*)malloc(sizeof(struct Queue));
-    queue->front = NULL;
-    queue->rear = NULL;
-    return queue;
+// Function to create an empty queue
+void createQueue(struct Queue* queue) {
+    queue->front = -1;  // Initialize front index
+    queue->rear = -1;   // Initialize rear index
 }
 
-void enqueue(struct Queue* queue, int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->next = NULL;
+// Function to check if the queue is empty
+int isEmpty(struct Queue* queue) {
+    return (queue->front == -1);
+}
 
-    if (queue->rear == NULL) {
-        queue->front = newNode;
-        queue->rear = newNode;
-    } else {
-        queue->rear->next = newNode;
-        queue->rear = newNode;
+// Function to check if the queue is full
+int isFull(struct Queue* queue) {
+    return ((queue->rear + 1) % MAX_SIZE == queue->front);
+}
+
+// Function to enqueue (add) an element to the queue
+void enqueue(struct Queue* queue, int data) {
+    // Check if the queue is full
+    if (isFull(queue)) {
+        printf("Queue is full. Cannot enqueue.\n");
+        return;
+    }
+
+    // Update the rear index and add the element to the rear position
+    queue->rear = (queue->rear + 1) % MAX_SIZE;
+    queue->list[queue->rear] = data;
+
+    // If it's the first element, update the front index as well
+    if (queue->front == -1) {
+        queue->front = queue->rear;
     }
 
     printf("%d enqueued to the queue.\n", data);
 }
 
+// Function to dequeue (remove) an element from the queue
 void dequeue(struct Queue* queue) {
-    if (queue->front == NULL) {
+    // Check if the queue is empty
+    if (isEmpty(queue)) {
         printf("Queue is empty. Cannot dequeue.\n");
         return;
     }
 
-    struct Node* temp = queue->front;
-    queue->front = queue->front->next;
+    // Store the element to be dequeued
+    int data = queue->list[queue->front];
 
-    if (queue->front == NULL) {
-        queue->rear = NULL;
+    // If there is only one element, reset both front and rear indexes
+    if (queue->front == queue->rear) {
+        queue->front = -1;
+        queue->rear = -1;
+    } else {
+        // Otherwise, move the front index to the next position
+        queue->front = (queue->front + 1) % MAX_SIZE;
     }
 
-    printf("%d dequeued from the queue.\n", temp->data);
-    free(temp);
+    printf("%d dequeued from the queue.\n", data);
 }
 
+// Function to display the elements in the queue
 void display(struct Queue* queue) {
-    if (queue->front == NULL) {
+    // Check if the queue is empty
+    if (isEmpty(queue)) {
         printf("Queue is empty.\n");
         return;
     }
 
-    struct Node* current = queue->front;
+    // Start from the front index and traverse to the rear index
+    int current = queue->front;
     printf("Queue: ");
-    while (current != NULL) {
-        printf("%d ", current->data);
-        current = current->next;
+    while (current != queue->rear) {
+        printf("%d ", queue->list[current]);
+        current = (current + 1) % MAX_SIZE;
     }
-    printf("\n");
+    printf("%d\n", queue->list[queue->rear]);
 }
 
 int main() {
-    struct Queue* queue = createQueue();
+    struct Queue queue;
 
-    enqueue(queue, 5);     // Enqueue 5
-    enqueue(queue, 10);    // Enqueue 10
-    enqueue(queue, 15);    // Enqueue 15
+    createQueue(&queue);
 
-    display(queue);        // Output: Queue: 5 10 15
+    enqueue(&queue, 5);     // Enqueue 5
+    enqueue(&queue, 10);    // Enqueue 10
+    enqueue(&queue, 15);    // Enqueue 15
 
-    dequeue(queue);        // Dequeue
-    dequeue(queue);        // Dequeue
+    display(&queue);        // Output: Queue: 5 10 15
 
-    display(queue);        // Output: Queue: 15
+    dequeue(&queue);        // Dequeue
+    dequeue(&queue);        // Dequeue
+
+    display(&queue);        // Output: Queue: 15
 
     return 0;
-    
 }
